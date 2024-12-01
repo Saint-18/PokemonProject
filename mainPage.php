8aa1b2f8f6fd86027d7a92a832c4e88c21825d3c
EmeraldSpringRain <162007114+EmeraldSpringRain@users.noreply.github.com>
2024-11-30 20:18:43 -0600

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -1,122 +1,153 @@
+
 <!-- 
+
 Sources:
 
 Double column layout: https://www.w3schools.com/howto/howto_css_two_columns.asp
 List filters: https://www.w3schools.com/howto/howto_js_filter_lists.asp
 Classes reference for .teamMember class: https://www.w3schools.com/html/html_classes.asp
 
--->
+ -->
 
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>Main Page</title>
 
 <meta charset="UTF-8">
-<meta name="author" content="Brandon Yates & Eli Ammons" >
+<meta name="author" content="Brandon Yates & Eli Ammons">
 
 <link rel="stylesheet" href="style.css" />
-    
+
 </head>
 <body>
 
-    <div class="top-bar">
-        <h1 class="header">
-Pokemon Team Builder
-        </h1>
-        
-    </div>
-    
-    <div class="row">
-    <div class="column" id="Roster">
-    <h2>
-    Your Team
-    <div class="btn-group">
-    <button>Save</button>
-    <button>Load</button>
-    </div>
-    </h2>
-    <div class="teamMember" id=1 data-inUse=true>
-                <h3>Pokemon 1</h3>
-                Some text..<br>
-                Some more text..<br>
-                Some more text..<br>
-                Some more text..
-            </div>
-            <div class="teamMember" id=2 data-inUse=true> 
-                <h3>Pokemon 2</h3>
-                Some text..<br>
-                Some more text..<br>
-                Some more text..<br>
-                Some more text..
-         </div>
-         <div class="teamMember" id=3 data-inUse=false> 
-                <h3>Empty</h3>
-         </div>
-         <div class="teamMember" id=4 data-inUse=false> 
-                <h3>Empty</h3>
-         </div>
-         <div class="teamMember" id=5 data-inUse=false> 
-                <h3>Empty</h3>
-         </div>
-         <div class="teamMember" id=6 data-inUse=false> 
-                <h3>Empty</h3>
-         </div>
-</div>
-        <div class="column" id="Search">
-            <h2>
-            Add Pokemon
-            <div class="btn-group">
-                <button>Search</button>
-                <button>Type Chart</button>
-                <button>Analysis</button>
-                <button>Help</button>
-                </div>
-            </h2>
-            <p>Search for a Pokemon to add using the box below.</p>
-            
-            <input type="text" id="searchInput" onkeyup="listFilter()" placeholder="Search for Pokemon..">
-    
-            <ul id="Pokedex">
-            <?php
-                // Database credentials
-                $servername = "localhost";
-                $username = "root";
-                $password = "Zekrom18!?!";
-                $dbname = "pokemon-database";
-                
-                // Create connection
-                $conn = mysqli_connect($servername, $username, $password, $dbname);
-                
-                // Check connection
-                if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
-                //echo "Connected successfully <br>";
-                $sql = "SELECT pokemon.id, pokemon.identifier AS pokemon_name, types.identifier AS type_name
-                FROM
-                    pokemon
-                INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id
-                INNER JOIN types ON pokemon_types.type_id = types.id
-                ORDER BY pokemon.id;";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "<li>". $row["pokemon_name"]. "   " . $row["type_name"]. "</li>";
-                    }
-                }  
-                // Close connection
-                mysqli_close($conn);
-            ?>
-            </ul> 
-        </div>
-    </div>
+	<div class="top-bar">
+		<h1 class="header">Pokemon Team Builder</h1>
+
+	</div>
+
+	<div class="row">
+		<div class="column" id="Roster">
+			<div class="heading-with-buttons">
+				<h2>Your Team</h2>
+				<div class="btn-group">
+					<button>Save</button>
+					<button>Load</button>
+				</div>
+			</div>
+			<div class="teamMember" id=1 data-inUse=true>
+				<h3>Pokemon 1</h3>
+				Some text..<br> Some more text..<br> Some more text..<br> Some more
+				text..
+			</div>
+			<div class="teamMember" id=2 data-inUse=true>
+				<h3>Pokemon 2</h3>
+				Some text..<br> Some more text..<br> Some more text..<br> Some more
+				text..
+			</div>
+			<div class="teamMember" id=3 data-inUse=true>
+				<div class="heading-with-buttons">
+					<h3>Jigglypuff</h3>
+					<div class="btn-group">
+						<button>Edit</button>
+						<button>Remove</button>
+					</div>
+				</div>
+
+				<div class="four-column-layout">
+					<div class="pokemon-column">
+						Pokedex #039<br> Gender: F<br> Type 1: Normal<br> Type 2: Fairy<br>
+						Held Item: Leftovers<br> Ability: Cute Charm<br> Nature: Serious
+					</div>
+					<div class="pokemon-column">
+						Blizzard<br> Body Slam<br> Fire Blast<br> Psychic
+					</div>
+					<div class="pokemon-column">
+						BST: 270<br> HP: 115<br> Attack: 45<br> Defense: 20<br> Sp. Atk.:
+						45<br> Sp. Def.: 25<br> Speed: 20
+					</div>
+					<div class="pokemon-column">
+						EVs/IVs<br> HP: 252/31<br> Attack: 4/31<br> Defense: 0/31<br> Sp.
+						Atk.: 252/31<br> Sp. Def.: 0/31<br> Speed: 0/31
+					</div>
+				</div>
+			</div>
+			<div class="teamMember" id=4 data-inUse=false>
+				<h3>Empty</h3>
+			</div>
+			<div class="teamMember" id=5 data-inUse=false>
+				<h3>Empty</h3>
+			</div>
+			<div class="teamMember" id=6 data-inUse=false>
+				<h3>Empty</h3>
+			</div>
+		</div>
+		<div class="column" id="Modular">
+			<div class="heading-with-buttons">
+				<h2>Add Pokemon</h2>
+				<div class="btn-group">
+					<button onclick="loadContent('search')">Search</button>
+					<button onclick="loadContent('type_chart')">Type Chart</button>
+					<button onclick="loadContent('analysis')">Analysis</button>
+					<button onclick="loadContent('help')">Help</button>
+				</div>
+			</div>
+
+			<div id="content-container">
+
+				<p>Search for a Pokemon to add using the box below.</p>
+
+				<input type="text" id="searchInput" onkeyup="listFilter()"
+					placeholder="Search for Pokemon..">
+
+				<ul id="Pokedex">
+
+					<li>
+						<div id="search-name">Bulbasaur</div>
+						<div id="search-type">Grass/Poison</div>
+					</li>
+					<li>
+						<div id="search-name">Ivysaur</div>
+						<div id="search-type">Grass/Poison</div>
+					</li>
+					<li>
+						<div id="search-name">Venasaur</div>
+						<div id="search-type">Grass/Poison</div>
+					</li>
+					<li>
+						<span id="pokemon-name">Pikachu</span>
+						<span id="type-name">Electric</span>
+					</li>
+					<li>
+						<span id="pokemon-name">Raichu</span>
+						<span id="type-name">Electric</span>
+					</li>
+					<li>
+						<div id="search-name">Jigglypuff</div>
+						<div id="search-type">Normal/Fairy</div>
+					</li>
+					<li>
+						<div id="search-name">Wigglytuff</div>
+						<div id="search-type">Normal/Fairy</div>
+					</li>
+				
+					<?php
+                    // require_once 'database_functions.php'; // Reference file for all database functions
+                    // getPokemonList();
+                    ?>
+				
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
 </body>
 
 <script src="functions.js"></script>
+<script src="pageUpdates.js"></script>
 
 </html>
-
-
