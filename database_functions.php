55de0933358ed6bf83087985794680fbed8f1b15
Saint-18 <67234406+Saint-18@users.noreply.github.com>
2024-12-02 13:09:18 -0600

    Add files via upload
    Added getters for stats and types.

--- database_functions.php
+++ database_functions.php
@@ -45,10 +45,9 @@ function getPokemonList()
     $previousPokemon = "";
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            if($previousPokemon == $row["pokemon_name"]){
+            if ($previousPokemon == $row["pokemon_name"]) {
                 echo "/ " . $row["type_name"] . "</li>";
-            }
-            else{
+            } else {
                 echo "<li>" . $row["pokemon_name"] . " " . $row["type_name"] . " ";
             }
             $previousPokemon = $row["pokemon_name"];
@@ -72,6 +71,247 @@ function getPokemonDetails($pokemonID)
     // TODO: Parse the $result variable OR return it to be parsed elsewhere
 
     dbCloseConnection($conn); // Close the connection
+}
+
+function getPokemonTypeOne($pokemonID)
+{
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $type = "";
+    $sql = "SELECT pokemon.id, pokemon.identifier AS pokemon_name, types.identifier AS type_name
+	FROM
+	    pokemon
+	INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id
+	INNER JOIN types ON pokemon_types.type_id = types.id
+    WHERE slot = 0
+	ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $type = $row["type_name"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $type;
+}
+
+function getPokemonTypeTwo($pokemonID)
+{
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $type = "";
+    $sql = "SELECT pokemon.id, pokemon.identifier AS pokemon_name, types.identifier AS type_name
+	FROM
+	    pokemon
+	INNER JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id
+	INNER JOIN types ON pokemon_types.type_id = types.id
+    WHERE slot = 1
+	ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $type = $row["type_name"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $type;
+}
+function getPokemonStats($pokemonID){
+    $hp = getPokemonHealth($pokemonID);
+    $Attack = getPokemonAttack($pokemonID);
+    $Defense = getPokemonDefense($pokemonID);
+    $SpecialAttack = getPokemonSpecialAttack($pokemonID);
+    $SpecialDefense = getPokemonSpecialDefense($pokemonID);
+    $Speed = getPokemonSpeed($pokemonID);
+    
+    $StatArray = array($hp, $Attack, $Defense, $SpecialAttack, $SpecialDefense, $Speed); 
+    return $StatArray;
+    
+}
+function getPokemonHealth($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+	FROM
+	    pokemon
+	INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+	INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 1
+	ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonAttack($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+    FROM
+    pokemon
+    INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+    INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 2
+    ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonDefense($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+    FROM
+    pokemon
+    INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+    INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 3
+    ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonSpecialAttack($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+    FROM
+    pokemon
+    INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+    INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 4
+    ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonSpecialDefense($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+    FROM
+    pokemon
+    INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+    INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 5
+    ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonSpeed($pokemonID)
+{
+    $stat = "";
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+    $sql = "SELECT pokemon.identifier AS pokemon_name, pokemon.id, pokemon_stats.base_stat,  stats.identifier AS pokemon_stat
+    FROM
+    pokemon
+    INNER JOIN pokemon_stats ON pokemon.id = pokemon_stats.pokemon_id
+    INNER JOIN stats ON stats.id = pokemon_stats.stat_id
+    Where stats.id = 6
+    ORDER BY pokemon.id;";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($pokemonID == $row["pokemon_name"]) {
+                $stat = $row["base_stat"];
+            }
+        }
+    }
+    dbCloseConnection($conn); // Close the connection
+    return $stat;
+}
+
+function getPokemonMoves($pokemonID)
+{
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+
+    dbCloseConnection($conn); // Close the connection
+    return " ";
+}
+
+function getPokemonAbilities($pokemonID)
+{
+    $conn = dbOpenConnection(); // Open the connection
+    if (! $conn) {
+        exit(); // No connection, exit the function
+    }
+
+    dbCloseConnection($conn); // Close the connection
+    return " ";
 }
 
 ?>
